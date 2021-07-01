59f112f6bf6054c275d0ac666e127fdf12488e7b
shahboura <shehabelhadidy@hotmail.com>
2021-07-01 15:08:49 +0800

    closes #2 changing solution structure to more ddd friendly

---
+++ src/Invoicing/Invoicing.Infrastructure/InvoiceRepository.cs
@@ -0,0 +1,22 @@
+namespace Invoicing.Infrastructure
+{
+    public class InvoiceRepository
+    {
+        private Invoice _invoice;
+
+        public Invoice GetInvoice(string reference)
+        {
+            return _invoice;
+        }
+
+        public void SaveInvoice(Invoice invoice)
+        {
+            //saves the invoice to the database
+        }
+
+        public void Add(Invoice invoice)
+        {
+            _invoice = invoice;
+        }
+    }
+}
---
+++ src/Invoicing/Invoicing.Infrastructure/Payment.cs
@@ -0,0 +1,8 @@
+namespace Invoicing.Infrastructure
+{
+    public class Payment
+    {
+        public decimal Amount { get; set; }
+        public string Reference { get; set; }
+    }
+}
