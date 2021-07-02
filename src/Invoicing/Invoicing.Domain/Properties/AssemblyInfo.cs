d960d39a31d750bd80d01df1cd7cb8a5c01599ce
shahboura <shehabelhadidy@hotmail.com>
2021-07-02 08:30:59 +0800

    - update AssemblyInfo

--- src/Invoicing/Invoicing.Domain/Properties/AssemblyInfo.cs
+++ src/Invoicing/Invoicing.Domain/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RefactorThis.Domain")]
+[assembly: AssemblyTitle("Invoicing.Domain")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RefactorThis.Domain")]
+[assembly: AssemblyProduct("Invoicing.Domain")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
