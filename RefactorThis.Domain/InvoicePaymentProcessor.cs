ced8925f25fe8d3c1f0cbc8de1e8b39e69815f6e
patelm369 <manojpatel369@gmail.com>
2021-07-15 12:50:27 +1200

    Update InvoicePaymentProcessor.cs
    Refactor - Process Payment - Manoj Patel

--- RefactorThis.Domain/InvoicePaymentProcessor.cs
+++ RefactorThis.Domain/InvoicePaymentProcessor.cs
@@ -12,83 +12,92 @@ namespace RefactorThis.Domain
 		{
 			_invoiceRepository = invoiceRepository;
 		}
-
-		public string ProcessPayment( Payment payment )
+		/// <summary>
+		/// Process payment
+		/// </summary>
+		/// <param name="payment"></param>
+		/// <returns></returns>
+		public string ProcessPayment(Payment payment)
 		{
-			var inv = _invoiceRepository.GetInvoice( payment.Reference );
+			var inv = Check_Invoice(_invoiceRepository.GetInvoice(payment.Reference));
 
-			var responseMessage = string.Empty;
+			if (inv.Amount == 0 && inv.Payments != null && inv.Payments.Any())
+				throw new InvalidOperationException(ErrorMessages.DATA_INTEGRITY_INVALID_INVOICE_AMOUNT_0);
 
-			if ( inv == null )
+			return Check_Invoice_Payment_Processing(inv, payment);
+
+		}
+
+		#region Invoice Processing methods
+	
+		private Invoice Check_Invoice(Invoice inv)
+		{
+			if (inv == null)
+				throw new InvalidOperationException(ErrorMessages.DATA_INTEGRITY_NO_INVOICE_MATCH);
+			else
+				return inv;
+		}
+		
+		private string Check_Invoice_Payment_Processing(Invoice invoice, Payment payment)
+		{
+			string message = "";
+			if(invoice.Amount== 0)
+				message = ResponseMessages.INVOICE_NO_PAYMENT_NEEDED;
+			else if (invoice.Payments != null && invoice.Payments.Any())
+				CheckPartialPayment(invoice, payment);
+			else
+				CheckOtherPayment(invoice, payment);
+			return message;
+		}
+	
+		private string CheckPartialPayment(Invoice invoice, Payment payment)
+        {
+			string message = "";
+			if (invoice.Payments.Sum(x => x.Amount) == invoice.Amount)
+				message = ResponseMessages.INVOICE_ALREADY_PAID;
+			else if (payment.Amount > (invoice.Amount - invoice.Payments.Sum(x => x.Amount)))
+				message = ResponseMessages.INVOICE_PAID_GREATER_THEN_REMAINING;
+			else if ((invoice.Amount - invoice.AmountPaid) == payment.Amount)
 			{
-				throw new InvalidOperationException( "There is no invoice matching this payment" );
+				UpdateInvoicePayment(invoice, payment);
+				message = ResponseMessages.INVOICE_AMOUNT_PARTIAL_PAYMENT_FULLY_PAID;
 			}
 			else
 			{
-				if ( inv.Amount == 0 )
-				{
-					if ( inv.Payments == null || !inv.Payments.Any( ) )
-					{
-						responseMessage = "no payment needed";
-					}
-					else
-					{
-						throw new InvalidOperationException( "The invoice is in an invalid state, it has an amount of 0 and it has payments." );
-					}
-				}
-				else
-				{
-					if ( inv.Payments != null && inv.Payments.Any( ) )
-					{
-						if ( inv.Payments.Sum( x => x.Amount ) != 0 && inv.Amount == inv.Payments.Sum( x => x.Amount ) )
-						{
-							responseMessage = "invoice was already fully paid";
-						}
-						else if ( inv.Payments.Sum( x => x.Amount ) != 0 && payment.Amount > ( inv.Amount - inv.AmountPaid ) )
-						{
-							responseMessage = "the payment is greater than the partial amount remaining";
-						}
-						else
-						{
-							if ( ( inv.Amount - inv.AmountPaid ) == payment.Amount )
-							{
-								inv.AmountPaid += payment.Amount;
-								inv.Payments.Add( payment );
-								responseMessage = "final partial payment received, invoice is now fully paid";
-							}
-							else
-							{
-								inv.AmountPaid += payment.Amount;
-								inv.Payments.Add( payment );
-								responseMessage = "another partial payment received, still not fully paid";
-							}
-						}
-					}
-					else
-					{
-						if ( payment.Amount > inv.Amount )
-						{
-							responseMessage = "the payment is greater than the invoice amount";
-						}
-						else if ( inv.Amount == payment.Amount )
-						{
-							inv.AmountPaid = payment.Amount;
-							inv.Payments.Add( payment );
-							responseMessage = "invoice is now fully paid";
-						}
-						else
-						{
-							inv.AmountPaid = payment.Amount;
-							inv.Payments.Add( payment );
-							responseMessage = "invoice is now partially paid";
-						}
-					}
-				}
+				UpdateInvoicePayment(invoice, payment);
+				message = ResponseMessages.INVOICE_AMOUNT_PAID_PARTIALLY;
 			}
-			
-			inv.Save();
 
-			return responseMessage;
+			return message;
 		}
+
+		private string CheckOtherPayment(Invoice invoice, Payment payment)
+        {
+			string message = "";
+			if (payment.Amount > invoice.Amount)
+				message = ResponseMessages.INVOICE_PAID_GREATER_THEN_TOTAL;
+			else if (invoice.Amount == payment.Amount)
+			{
+				UpdateInvoicePayment(invoice, payment);
+				message = ResponseMessages.INVOICE_PAID_FULLY;
+			}
+			else
+			{
+				UpdateInvoicePayment(invoice, payment);
+				message = ResponseMessages.INVOICE_PAID_PARTIALLY;
+			}
+			return message;
+		}
+		
+		private void UpdateInvoicePayment(Invoice inv, Payment payment)
+		{
+			inv.AmountPaid += payment.Amount;
+			inv.Payments.Add(payment);
+			inv.Save();
+		}
+
+		
+		#endregion
+
 	}
 }
