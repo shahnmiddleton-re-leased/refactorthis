35f320438332138b983ad77d1ce0097b9efca61a
Greg Holmes <greg320@hotmail.com>
2021-07-23 19:36:07 +1200

    Refactored Invoice Payment Processor

--- RefactorThis.Persistence/Invoice.cs
+++ RefactorThis.Persistence/Invoice.cs
@@ -1,22 +1,81 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RefactorThis.Persistence
 {
-	public class Invoice
-	{
-		private readonly InvoiceRepository _repository;
-		public Invoice( InvoiceRepository repository )
-		{
-			_repository = repository;
-		}
+    /// <summary>
+    /// An invoice, which has an <see cref="InvoiceNumber"/> and list of <see cref="Payment"/>s
+    /// </summary>
+    public class Invoice
+    {
+        private readonly List<Payment> _payments;
 
-		public void Save( )
-		{
-			_repository.SaveInvoice( this );
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Invoice"/> class.
+        /// </summary>
+        /// <param name="invoiceNumber">The invoice number.</param>
+        /// <param name="amount">The amount.</param>
+        public Invoice(string invoiceNumber, decimal amount)
+        {
+            _payments = new List<Payment>();
 
-		public decimal Amount { get; set; }
-		public decimal AmountPaid { get; set; }
-		public List<Payment> Payments { get; set; }
-	}
+            InvoiceNumber = invoiceNumber;
+            Amount = amount;
+        }
+
+        /// <summary>
+        /// Gets the invoice number.
+        /// </summary>
+        /// <value>The invoice number.</value>
+        public string InvoiceNumber { get; }
+
+        /// <summary>
+        /// Gets the amount.
+        /// </summary>
+        /// <value>The amount.</value>
+        public decimal Amount { get; }
+
+        /// <summary>
+        /// Gets the amount paid.
+        /// </summary>
+        /// <value>The amount paid.</value>
+        public decimal AmountPaid => Payments.Sum(x => x.Amount);
+
+        /// <summary>
+        /// Gets the balance.
+        /// </summary>
+        /// <value>The balance.</value>
+        public decimal Balance => Amount - AmountPaid;
+
+        /// <summary>
+        /// Gets the payments.
+        /// </summary>
+        /// <value>The payments.</value>
+        public IReadOnlyCollection<Payment> Payments => _payments;
+
+        /// <summary>
+        /// Makes a payment
+        /// </summary>
+        /// <param name="payment">The payment.</param>
+        public void MakePayment(Payment payment)
+        {
+            _payments.Add(payment);
+
+			// TODO: Log this to a central repository
+			Console.WriteLine($"A payment of ${payment.Amount} has been made, with reference '{payment.PaymentReference}'");
+        }
+
+        /// <summary>
+        /// Reverses a payment
+        /// </summary>
+        /// <param name="payment">The payment.</param>
+        public void ReversePayment(Payment payment)
+        {
+            _payments.Remove(payment);
+
+            // TODO: Log this to a central repository
+            Console.WriteLine($"A payment of ${payment.Amount}, with reference '{payment.PaymentReference}' has been reversed and should be investigated");
+        }
+    }
 }
--- RefactorThis.Persistence/InvoiceRepository.cs
+++ RefactorThis.Persistence/InvoiceRepository.cs
@@ -1,21 +1,97 @@
-namespace RefactorThis.Persistence {
-	public class InvoiceRepository
-	{
-		private Invoice _invoice;
+using System;
+using System.Collections.Generic;
 
-		public Invoice GetInvoice( string reference )
-		{
-			return _invoice;
-		}
+namespace RefactorThis.Persistence 
+{
+    /// <summary>
+    /// An in-memory collection of <see cref="Invoice"/>s
+    /// </summary>
+    public class InvoiceRepository
+    { 
+        private readonly Dictionary<string, Invoice> _invoices;
 
-		public void SaveInvoice( Invoice invoice )
-		{
-			//saves the invoice to the database
-		}
+        /// <summary>
+        /// Gets the invoice by invoice number.
+        /// </summary>
+        /// <param name="invoiceNumber">The invoice number.</param>
+        /// <returns>The invoice if found, otherwise <c>null</c></returns>
+        public Invoice GetInvoiceByInvoiceNumber(string invoiceNumber)
+        {
+            return _invoices.ContainsKey(invoiceNumber) ? _invoices[invoiceNumber] : null;
+        }
 
-		public void Add( Invoice invoice )
+        /// <summary>
+        /// Saves the invoice to the database.
+        /// </summary>
+        /// <param name="invoice">The invoice.</param>
+        /// <returns><c>true</c> if saved, <c>false</c> otherwise.</returns>
+        public bool SaveInvoice(Invoice invoice)
 		{
-			_invoice = invoice;
-		}
-	}
+            try
+            {
+                if (_invoices.ContainsKey(invoice.InvoiceNumber))
+                {
+                    _invoices[invoice.InvoiceNumber] = invoice;
+
+                    // TODO: UPDATE Invoice in database
+                }
+                else
+                {
+                    _invoices.Add(invoice.InvoiceNumber, invoice);
+
+                    // TODO: INSERT Invoice into database
+                }
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log this appropriately
+                Console.WriteLine($"Failed to save new invoice to database - {ex.Message}");
+
+                return false;
+            }
+        }
+
+        private static IEnumerable<Invoice> LoadExistingInvoices()
+        {
+			// TODO: SELECT these from the database
+            return new List<Invoice>();
+        }
+
+        #region Singleton Instance
+
+        private static readonly object _singletonInstanceLock = new object();
+        private static InvoiceRepository _instance;
+
+        public static InvoiceRepository Instance
+        {
+            get
+            {
+                lock (_singletonInstanceLock)
+                {
+                    if (_instance is null)
+                    {
+                        _instance = new InvoiceRepository();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private InvoiceRepository()
+        {
+            _invoices = new Dictionary<string, Invoice>();
+
+            var databaseInvoices = LoadExistingInvoices();
+
+            foreach (var databaseInvoice in databaseInvoices)
+            {
+                _invoices.Add(databaseInvoice.InvoiceNumber, databaseInvoice);
+            }
+        }
+
+        #endregion
+    }
 }
