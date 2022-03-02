43d04b7d143d92321b9aa78fa1f7a17fdd31c70e
NEVIN-DEV\Nevin <nevin.ma@processonesolutions.com>
2022-03-02 17:22:30 +1300

    Refactoring done, Testing ongoing

---
+++ RefactorThis.Domain/PaymentHandlers/PartialPaymentHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace RefactorThis.Domain.PaymentHandlers
+{
+    public class PartialPaymentHandler : IPaymentHandler
+    {
+        public void Handle(Payload payload)
+        {
+            if (payload.SkipRestHandlers) return;
+
+            if (!payload.InvoiceAlreadyHasPayment)
+            {
+                return;
+            }
+            else
+            {
+                switch (payload.CurrentInvoiceStatus)
+                {
+                    case CurrentInvoiceStatus.ExceedInvoiceAmount:
+                        payload.Response = "the payment is greater than the partial amount remaining";
+                        break;
+                    case CurrentInvoiceStatus.FullyPaid:
+                        payload.Response = "final partial payment received, invoice is now fully paid";
+                        break;
+                    case CurrentInvoiceStatus.PartialPaid:
+                        payload.Response = "another partial payment received, still not fully paid";
+                        break;
+                    default:
+                        break;
+                };
+            }
+        }
+    }
+}
