c202f035c7b1ac28cc1460e791b9634f83554e35
Jesse Drummond <jesse_drummond@hotmail.com>
2022-06-19 19:09:06 +1200

    Format all .cs files

--- RefactorThis.Persistence/Invoice.cs
+++ RefactorThis.Persistence/Invoice.cs
@@ -2,21 +2,21 @@ using System.Collections.Generic;
 
 namespace RefactorThis.Persistence
 {
-	public class Invoice
-	{
-		private readonly InvoiceRepository _repository;
-		public Invoice( InvoiceRepository repository )
-		{
-			_repository = repository;
-		}
+    public class Invoice
+    {
+        private readonly InvoiceRepository _repository;
+        public Invoice(InvoiceRepository repository)
+        {
+            _repository = repository;
+        }
 
-		public void Save( )
-		{
-			_repository.SaveInvoice( this );
-		}
+        public void Save()
+        {
+            _repository.SaveInvoice(this);
+        }
 
-		public decimal Amount { get; set; }
-		public decimal AmountPaid { get; set; }
-		public List<Payment> Payments { get; set; }
-	}
+        public decimal Amount { get; set; }
+        public decimal AmountPaid { get; set; }
+        public List<Payment> Payments { get; set; }
+    }
 }
